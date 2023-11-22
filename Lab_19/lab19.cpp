543f7fa6ae3720026db4c51906b5d296c791b1a6
andrew <you@example.com>
2023-11-22 14:18:48 +0200

    complete lab 19

---
+++ Lab_19/lab19.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <math.h>
+#include <iomanip>
+using namespace std;
+int main()
+{
+    float arr[4][4], akn;
+    int counter = 0, counter_1 = 0;
+    for (size_t i = 0; i < 4; i++)
+    {
+        for (size_t j = 0; j < 4; j++)
+        {
+            akn = j * abs(sin(pow(j, 2) / 1.5 - 2)) + 11.73 * cos(1.6 * j - 1) + sin(j) * 13.4 * cos(fabs(j)) * sin((pow(j, 2) - 2.25));
+            arr[i][j] = akn;
+            //^--- заповнення масиву формулою
+            if (akn > 3)
+            {
+                ++counter;
+            }
+            else if (akn < 9)
+            {
+                ++counter_1;
+            }
+            //^--- перевірка
+        }
+    }
+    for (int i = 0; i < 4; ++i)
+    {
+        for (int j = 0; j < 4; ++j)
+        {
+            cout << setprecision(3) << arr[i][j] << "\t";
+        }
+        cout << endl;
+    }
+    //^--- вивід масиву
+    cout << "Елементи масиву більше 3 : " << counter << "\nЕлементи масиву меньше 9 : " << counter_1 << endl;
+}
